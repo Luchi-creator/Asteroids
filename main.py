2db4acbd557261b29a40f03550b9fb4138bdbb04
Luchi-creator <ionut.grigore.luchian@gmail.com>
2025-07-30 21:58:40 +0300

    The player is now showing on the main screen

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
+
     clock = pygame.time.Clock()
     dt = 0
     
@@ -15,7 +18,9 @@ def main():
             
         dt = clock.tick(60)/1000
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
