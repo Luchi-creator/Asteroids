7592406a726696e762a0237b6e65858c6c2d84b1
Luchi-creator <ionut.grigore.luchian@gmail.com>
2025-07-30 21:40:24 +0300

    Added CirclesShape class for future development

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        dt = clock.tick(60)/1000
         screen.fill("black")
         pygame.display.flip()
 
